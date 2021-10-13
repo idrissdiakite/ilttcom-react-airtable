6b118c8d78fc90905a36cb0bda37b3e114fa688f
idrissdiakite <idriss.diakite@hotmail.fr>
2021-10-13 14:42:24 +0200

    motion

--- src/components/Music/Filter.js
+++ src/components/Music/Filter.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Filter = (props) => {
     const {genre, setSelectedGenre} = props;
 
   return (
-      <button
+      <motion.button
         type="button"
         id={genre}
         key={genre}
         value={genre}
         onClick={(e) => setSelectedGenre(e.currentTarget.value)}
         className="filter-btn"
+        transition={transition}
       >
         {genre}
-      </button>
+      </motion.button>
   );
+};
+
+const transition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 2.5,
 };
 
 export default Filter;
