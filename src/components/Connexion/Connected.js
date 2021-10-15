be2f35adbadbcfe1a3cab798872da5cc13f51df8
idrissdiakite <idriss.diakite@hotmail.fr>
2021-10-15 09:34:27 +0200

    add redirection on signOut

--- src/components/Connexion/Connected.js
+++ src/components/Connexion/Connected.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useHistory } from "react-router-dom"
 import { firebase } from "../../utils/firebaseConfig";
 import Navigation from "../Navigation"
 import Footer from "../Footer";
 
 const Connected = () => {
 
+  const history = useHistory();
+
     const signOut= () => {
         firebase.auth().signOut();
+
+        let path = "/";
+        history.push(path);
     }
 
   return (
@@ -14,7 +20,7 @@ const Connected = () => {
       <Navigation />
       <section className="connected">
         <div className="container">
-          <h2>{firebase.auth().currentUser.displayName}, <br /> vous êtres déjà connecté !</h2>
+          <h2>Hello {firebase.auth().currentUser.displayName}! <br /></h2>
           <div>
           <button className="button" onClick={signOut}>Se déconnecter</button>
           </div>
