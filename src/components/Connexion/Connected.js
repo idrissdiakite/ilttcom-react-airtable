6220824ee198c1100104ca7bb9748a21251c1e5e
idrissdiakite <idriss.diakite@hotmail.fr>
2021-10-15 10:23:14 +0200

    motion framer

--- src/components/Connexion/Connected.js
+++ src/components/Connexion/Connected.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { useHistory } from "react-router-dom"
 import { firebase } from "../../utils/firebaseConfig";
 import Navigation from "../Navigation"
@@ -16,19 +17,54 @@ const Connected = () => {
     }
 
   return (
-    <>
+    <motion.div
+    initial="initial"
+      animate="in"
+      variants={variants}
+      transition={transition}>
       <Navigation />
-      <section className="connected">
+      <motion.section 
+      className="connected"
+      initial="initial"
+      animate="in"
+      variants={variants}
+      transition={containerTransition}>
         <div className="container">
           <h2>Hello {firebase.auth().currentUser.displayName}! <br /></h2>
           <div>
           <button className="button" onClick={signOut}>Se déconnecter</button>
           </div>
         </div>
-      </section>
+      </motion.section>
       <Footer />
-    </>
+    </motion.div>
   );
+};
+
+// Motion
+const variants = {
+  initial: {
+    opacity: 0,
+    scale: 1,
+    y: -100,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+  }
+};
+
+const transition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 1.5,
+};
+
+const containerTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 2,
 };
 
 export default Connected;
--- src/components/Connexion/Signin.js
+++ src/components/Connexion/Signin.js
@@ -1,52 +1,89 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { firebase } from "../../utils/firebaseConfig";
 import "firebase/compat/auth";
 import Navigation from "../Navigation";
 import Footer from "../Footer";
 
 const Signin = () => {
+  const signInWithGoogle = () => {
+    let google_provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(google_provider)
+      .then((re) => {
+        console.log(re);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const signInWithGoogle = () => {
-        let google_provider = new firebase.auth.GoogleAuthProvider();
-        firebase
-          .auth()
-          .signInWithPopup(google_provider)
-          .then((re) => {
-            console.log(re);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
-    
-      const signInWithFacebook = () => {
-        let facebook_provider = new firebase.auth.FacebookAuthProvider();
-        firebase
-          .auth()
-          .signInWithPopup(facebook_provider)
-          .then((re) => {
-            console.log(re);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+  const signInWithFacebook = () => {
+    let facebook_provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(facebook_provider)
+      .then((re) => {
+        console.log(re);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <>
+    <motion.div
+      initial="initial"
+      animate="in"
+      variants={variants}
+      transition={transition}
+    >
       <Navigation />
-      <section className="login">
+      <motion.section
+        className="login"
+        initial="initial"
+        animate="in"
+        variants={variants}
+        transition={containerTransition}
+      >
         <div className="container">
-          <h2>Connexion <br /> <i className="fas fa-long-arrow-alt-down"></i></h2>
+          <h2>
+            Connexion <br /> <i className="fas fa-long-arrow-alt-down"></i>
+          </h2>
           <button onClick={signInWithGoogle}>Google</button> <br />
-          <button onClick={signInWithFacebook}>
-            Facebook
-          </button>
+          <button onClick={signInWithFacebook}>Facebook</button>
         </div>
-      </section>
+      </motion.section>
       <Footer />
-    </>
+    </motion.div>
   );
+};
+
+// Motion
+const variants = {
+  initial: {
+    opacity: 0,
+    scale: 1,
+    y: -100,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+  },
+};
+
+const transition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 1.5,
+};
+
+const containerTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 2,
 };
 
 export default Signin;
